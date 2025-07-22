94be1fa60990c6c1b1bce7c46a5fcabd702395c0
NaughtyCoon <sterrestrian@gmail.com>
2025-07-23 02:43:58 +0300

    Completely functional

--- src/main/java/org/example/DiceTournamentAnalyzer.java
+++ src/main/java/org/example/DiceTournamentAnalyzer.java
@@ -1,18 +1,22 @@
 package org.example;
 
-import org.jooq.lambda.Seq;
-
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+/**
+ * Класс предназначен для анализа статистики игроков в кости и распределения их мест в турнирных таблицах.
+ */
 public class DiceTournamentAnalyzer {
 
     public enum ChipCategory { HIGH, MEDIUM, LOW }
 
-    public Map<Player, Double> mapOfAverageScores(List<Player> list) {
+    /**
+     * @param list коллекция игроков класса Player
+     * @return возвращает Map, ключом в которой является объект игрока класса Player,
+     * а значением - среднее значение его бросков.
+     */
+    public Map<Player, Double> getMapOfAverageScores(List<Player> list) {
         return
                 list.stream()
                         .collect(Collectors.toMap(
@@ -21,10 +25,15 @@ public class DiceTournamentAnalyzer {
                         ));
     }
 
-    public List<Player> topThreeAveragePlayers(List<Player> list) {
+    /**
+     * @param list коллекция игроков класса Player
+     * @return возвращает коллекцию из трёх самых успешных игроков,
+     * отсортированных по убыванию среднего значения бросков.
+     */
+    public List<Player> getTopThreeAveragePlayers(List<Player> list) {
 
         return
-                mapOfAverageScores(list).entrySet().stream()
+                getMapOfAverageScores(list).entrySet().stream()
                         .sorted(Map.Entry.<Player, Double>comparingByValue().reversed())
                         .limit(3)
                         .map(Map.Entry::getKey)
@@ -32,21 +41,37 @@ public class DiceTournamentAnalyzer {
 
     }
 
-    public List<Player> showTheLosers(List<Player> list) {
+    /**
+     * @param list коллекция игроков класса Player
+     * @return возвращает коллекцию из самых нерезультативных игроков
+     * (три и более раза выпадала единица).
+     */
+    public List<Player> getTheLosers(List<Player> list) {
         return
                 list.stream()
                         .filter(e->evaluateFate(e,1,3))
                         .toList();
     }
 
-    public List<Player> showTheLucky(List<Player> list) {
+    /**
+     * @param list коллекция игроков класса Player
+     * @return возвращает коллекцию из самых результативных игроков
+     * (четыре и более раза выпадала шестёрка).
+     */
+    public List<Player> getTheLucky(List<Player> list) {
         return
                 list.stream()
                         .filter(e->evaluateFate(e,6,4))
                         .toList();
     }
 
-    public Map<ChipCategory, List<Player>> groupPlayersByChips(List<Player> list) {
+    /**
+     * @param list коллекция игроков класса Player
+     * @return возвращает отсортированный по ключу объект LinkedHashMap, ключом в котором
+     * является категория количества фишек (>1000 - HIGH, 500-1000 - MEDIUM, <500 - LOW),
+     * а значением - коллекция игроков, попадающих в соответствующую категорию.
+     */
+    public Map<ChipCategory, List<Player>> getPlayersGroupedByChips(List<Player> list) {
 
         Map<ChipCategory, List<Player>> result = new LinkedHashMap<>();
 
@@ -74,7 +99,6 @@ public class DiceTournamentAnalyzer {
     }
 
     /**
-     *
      * @param list коллекция игроков класса Player
      * @return возвращает список игроков, которым 8 или более раз удалось получить значение
      * кубика 4 или больше.
@@ -88,16 +112,101 @@ public class DiceTournamentAnalyzer {
                         .toList();
     }
 
+    /**
+     * @param list коллекция игроков класса Player
+     * @return возвращает список игроков, у которых не менее двух раз подряд
+     * выпадала единица.
+     */
     public List<Player> getRiskyPlayerList(List<Player> list) {
-        return Seq.seq(list)
-                .filter(player -> Seq.seq(player.getRollHistory())
-                        .window(2)
-                        .anyMatch(window -> window.v1 == 1 && window.v2 == 1)
-                )
-                .toList();
+        return
+                list.stream()
+                        .filter(player -> {
+                            List<Integer> history = player.getRollHistory();
+                            return IntStream.range(0, history.size() - 1)
+                                    .anyMatch(i -> history.get(i) == 1 && history.get(i + 1) == 1);
+                        })
+                        .toList();
     }
 
-    // Служебный метод. Определяет среднее значение из всех бросков кубика для конкретного игрока.
+    /**
+     * @param list коллекция игроков класса Player
+     * @return возвращает список игроков, у которых каждая сторона кости
+     * выпадала хотя бы раз.
+     */
+    public List<Player> getBalancedDicePlayerList(List<Player> list) {
+        return
+                list.stream()
+                        .filter(player -> (player.getRollHistory().stream()
+                                .distinct()
+                                .toList()
+                                ).size() == 6)
+                        .toList();
+    }
+
+    /**
+     * @param list коллекция игроков класса Player
+     * @return возвращает Map, где ключами являются игроки, а значениями
+     * их "индексы удачи", округлённые до 1 знака после запятой.
+     */
+    public Map<Player, Double> getLuckIndex(List<Player> list) {
+
+        return
+                list.stream()
+                        .collect(Collectors.toMap(
+                                player -> player,
+                                player -> {
+
+                                    List<Integer> history = player.getRollHistory();
+
+                                    if (history.isEmpty()) {
+                                        return 0.0;
+                                    }
+
+                                    return
+                                            Math.round(
+                                            (history.stream()
+                                            .mapToInt(e -> e)
+                                            .sum() / 6.0 * player.getChips()))/10.0;
+
+                                }
+                        ));
+
+    }
+
+    /**
+     * @param list коллекция игроков класса Player
+     * @return возвращает игрока (объект Player) с самой большой дисперсией
+     * результатов бросков.
+     */
+    public Optional<Player> getMaxMathVariancePlayer(List<Player> list) {
+        return list.stream()
+                .max(Comparator.comparingDouble(this::calculateVariance));
+    }
+
+    /**
+     * Служебный метод.
+     * @param player объект класса Player, конкретный игрок.
+     * @return возвращает дисперсию конкретного игрока.
+     */
+    private double calculateVariance(Player player) {
+
+        List<Integer> history = player.getRollHistory();
+        if (history.isEmpty()) return 0.0;
+
+        double mu = playerAverageScore(player);
+
+        return history.stream()
+                .mapToDouble(e -> Math.pow(e - mu, 2))
+                .average()
+                .orElse(0.0);
+
+    }
+
+    /**
+     * Служебный метод.
+     * @param player объект класса Player, конкретный игрок.
+     * @return возвращает среднее значение из всех бросков кубика для конкретного игрока.
+     */
     private double playerAverageScore(Player player) {
 
         if (player.getRollHistory().isEmpty()) {
@@ -112,8 +221,14 @@ public class DiceTournamentAnalyzer {
 
     }
 
-    // Служебный метод. Определяет количество выпадения определённого значения кубика
-    // для заданного игрока
+    /**
+     * Служебный метод.
+     * @param player объект класса Player, конкретный игрок.
+     * @param value значение кубика.
+     * @param quantity проверяемое количество выпадений.
+     * @return возвращает true, если количество выпадений определённого значения кубика
+     * value равно или превосходит quantity.
+     */
     private boolean evaluateFate(Player player, int value, int quantity) {
 
         return
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import javax.xml.transform.Source;
 import java.util.List;
 
 public class Main {
@@ -32,39 +31,39 @@ public class Main {
         // Далее анализ через DiceTournamentAnalyzer...
 
         task("1.\tСредний результат бросков (Map<Player, Double>).");
-        System.out.println(dta.mapOfAverageScores(players));
+        System.out.println(dta.getMapOfAverageScores(players));
 
         task("2.\tТоп-3 игроков с самым высоким средним.");
-        System.out.println(dta.topThreeAveragePlayers(players));
+        System.out.println(dta.getTopThreeAveragePlayers(players));
 
         task("3.\t\"Неудачники\" – игроки, у которых ≥3 раза выпала 1.");
-        System.out.println(dta.showTheLosers(players));
+        System.out.println(dta.getTheLosers(players));
 
         task("4.\t\"Везунчики\" – игроки, у которых ≥4 раза выпала 6.");
-        System.out.println(dta.showTheLucky(players));
+        System.out.println(dta.getTheLucky(players));
 
         task("5.\tГруппировка игроков по фишкам (>1000, 500-1000, <500).");
-        System.out.println(dta.groupPlayersByChips(players));
+        System.out.println(dta.getPlayersGroupedByChips(players));
 
         task("6.\t\"Стабильные кости\" – у которых ≥8 из 10 бросков ≥4.");
         System.out.println(dta.getStablePlayerList(players));
 
         task("7.\t\"Рисковые игроки\" – у которых ≥2 раза подряд выпадала 1.");
-
+        System.out.println(dta.getRiskyPlayerList(players));
 
         task("8.\t\"Сбалансированные кости\" – все числа от 1 до 6 выпали хотя бы раз.");
-
+        System.out.println(dta.getBalancedDicePlayerList(players));
 
         task("9.\t\"Индекс удачи\" = (сумма бросков) / (6 × 10) × chips.");
-
+        System.out.println(dta.getLuckIndex(players));
 
         task("10.\tИгрок с наибольшей дисперсией.");
-
-
+        System.out.println(dta.getMaxMathVariancePlayer(players));
 
     }
 
     private static void task(String message) {
         System.out.println(GREEN + "\n" + message + RESET);
     }
+
 }
